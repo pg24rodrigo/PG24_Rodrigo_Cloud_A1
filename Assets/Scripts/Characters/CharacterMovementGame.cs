a7da151d5cae0e30cfd59901cff39bf3c7ed5883
rodrigoferzuli <rodrigoferzuli@gmail.com>
2023-07-31 18:02:15 -0700

    Class 6 updates.

--- Assets/Scripts/Characters/CharacterMovementGame.cs
+++ Assets/Scripts/Characters/CharacterMovementGame.cs
@@ -1,23 +1,49 @@
 using System;
 using System.Collections;
 using System.Collections.Generic;
+using System.ComponentModel;
+using System.Runtime.CompilerServices;
 using CharacterMovement;
 using UnityEngine;
 using UnityEngine.AI;
 using VFSCloud;
 
-    public class CharacterMovementGame : CharacterMovement3D, IRemotePlayer
+    public class CharacterMovementGame : CharacterMovement3D, INotifyPropertyChanged
     {
+        public PlayerData Stats;
+        
         public void Awake()
         {
             base.Awake();
+
+            UpdateFromStats();
         }
 
-        // From IRemoteData:
-        public void LoadConfigs(PlayerData theData)
+        public void OnPropertyChanged()
         {
-            _speed = theData.Speed;
-            _turnSpeed = theData.TurnSpeed;
-            _jumpHeight = theData.JumpHeight;
+            UpdateFromStats();
+        }
+
+        private void UpdateFromStats()
+        {
+            _speed = Stats.Speed;
+            _turnSpeed = Stats.TurnSpeed;
+            _jumpHeight = Stats.JumpHeight;
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        protected virtual void OnPropertyChanged([CallerMemberName] string propertyName = null)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+
+        protected bool SetField<T>(ref T field, T value,
+            [CallerMemberName] string propertyName = null)
+        {
+            if (EqualityComparer<T>.Default.Equals(field, value)) return false;
+            field = value;
+            OnPropertyChanged(propertyName);
+            return true;
         }
     }
--- Assets/Scripts/Remote/PlayerData.cs
+++ Assets/Scripts/Remote/PlayerData.cs
@@ -3,8 +3,9 @@ using UnityEngine;
 
 namespace VFSCloud
 {
-    [System.Serializable]
-    public class PlayerData
+    //[System.Serializable]
+    [CreateAssetMenu(fileName = "PlayerData", menuName = "CloudDemo/Player Data", order = 1)]
+    public class PlayerData : ScriptableObject, IRemoteData
     {
         [SerializeField] public float Speed;
         [SerializeField] public float TurnSpeed;
@@ -15,6 +16,13 @@ namespace VFSCloud
             Speed = 1.0f;
             TurnSpeed = 1.0f;
             JumpHeight = 1.0f;
+        }
+
+        public void UpdateConfig(PlayerData newData)
+        {
+            Speed = newData.Speed;
+            TurnSpeed = newData.TurnSpeed;
+            JumpHeight = newData.JumpHeight;
         }
     }
 }
