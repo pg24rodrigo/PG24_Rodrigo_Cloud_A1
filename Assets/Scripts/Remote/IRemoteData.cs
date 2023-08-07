855d1446b08be10a2701f3a9a1487f36ac567ddd
rodrigoferzuli <rodrigoferzuli@gmail.com>
2023-08-06 20:32:50 -0700

    Latest changes for A3 submission.

--- Assets/Scripts/Remote/IRemoteData.cs
+++ Assets/Scripts/Remote/IRemoteData.cs
@@ -2,17 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class IRemoteData : MonoBehaviour
+public interface IRemoteData
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
+    
 }
